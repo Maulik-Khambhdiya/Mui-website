7e3013b138826eca58e978b08c114d3c2719890a
Maulik-Khambhdiya <maulikkhambhdiya444@gmail.com>
2025-04-30 17:44:32 +0530

    website complete

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ import staircase from './image/staircase.jpg';
 import appposter from './image/appointment-poster.jpg';
 import blacksofa from './image/blacksofa.avif';
 import bluesofa from './image/bluesofa.avif';
-import redsofa from './image/redsofa.avif';
+import EmailIcon from '@mui/icons-material/Email';
 import creamsofa from './image/creamsofa.avif';
 import greensofa from './image/greensofa.webp';
 import Tooltip from '@mui/material/Tooltip';
@@ -53,10 +53,97 @@ import HomeIcon from '@mui/icons-material/Home';
 import PaymentIcon from '@mui/icons-material/Payment';
 import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import CallIcon from '@mui/icons-material/Call';
+import BusinessIcon from '@mui/icons-material/Business';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import disbanner from './image/banner.webp';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import img1 from './image/bluecupborad.jpg';
+import img2 from './image/img2.jpg';
+import img3 from './image/mirror.jpg';
+import img4 from './image/ac.jpg';
+import img5 from './image/sofachair.jpg';
+import img6 from './image/worktable.jpg';
+import img7 from './image/bedroomceiling.jpg';
+import img8 from './image/aerobed.jpg';
+import img9 from './image/bathtub.jpg';
+import img10 from './image/frame.jpg';
+import img11 from './image/bookstand.jpg';
+import img12 from './image/balcony.jpg';
+
 
 
 const pages = ['NEW', 'FURNITURE', 'LIGHTING', 'DECOR', 'COLLECTION', 'OUR STORY'];
-
+const itemData = [
+  {
+    img: img1 ,
+    title: 'Bed',
+    author: 'Pavel Nekoranec',
+    button: <button>Add to Cart</button>
+  },
+  {
+    img: img2,
+    title: 'Books',
+    author: 'Pavel Nekoranec',
+  },
+  {
+    img: img3,
+    title: 'Sink',
+    author: 'Charles Deluvio',
+  },
+  {
+    img: img4,
+    title: 'Kitchen',
+    author: 'Christian Mackie',
+  },
+  {
+    img: img5,
+    title: 'Blinds',
+    author: 'Darren Richardson',
+  },
+  {
+    img: img6,
+    title: 'Chairs',
+    author: 'Taylor Simpson',
+  },
+  {
+    img: img7,
+    title: 'Laptop',
+    author: 'Ben Kolde',
+  },
+  {
+    img: img8,
+    title: 'Doors',
+    author: 'Philipp Berndt',
+  },
+  {
+    img: img9,
+    title: 'Coffee',
+    author: 'Jen P.',
+  },
+  {
+    img: img10,
+    title: 'Storage',
+    author: 'Douglas Sheppard',
+  },
+  {
+    img: img11,
+    title: 'Candle',
+    author: 'Fi Bell',
+  },
+  {
+    img: img12,
+    title: 'Coffee table',
+    author: 'Hutomo Abrianto',
+  },
+];
 
 
 const App = () => {
@@ -206,7 +293,7 @@ const App = () => {
                 <Button
                   key={page}
                   onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'black', display: 'block', fontWeight: 'bold', borderRadius: '30px', border: "1px solid black", ":hover": { background: 'linear-gradient(to right, white, black)', color: 'white', borderRadius: '30px' }, fontFamily: 'sans-serif' }}
+                  sx={{ my: 2, color: 'black', display: 'block', fontWeight: 'bold', borderRadius: '0px', ":hover": { background: 'linear-gradient(to right, black,grey, black)', color: 'white', borderRadius: '0px' }, fontFamily: 'sans-serif' }}
 
                 >
                   {page}
@@ -634,29 +721,29 @@ const App = () => {
 
       {/* make an appointment section  start*/}
       <Box sx={{ margin: "100px 0" }}>
-      <Container>
+        <Container>
 
-        <Grid container spacing={2} sx={{ backgroundColor: "GrayText" }}>
+          <Grid container spacing={2} sx={{ backgroundColor: "GrayText" }}>
 
-          <Grid size={{lg: 7, xs: 12, md: 5, sm: 12 }}>
-            <img src={appposter} alt="" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+            <Grid size={{ lg: 7, xs: 12, md: 5, sm: 12 }}>
+              <img src={appposter} alt="" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+            </Grid>
+
+            <Grid size={{ lg: 5, xs: 12, md: 5, sm: 12 }} sx={{ margin: "auto 0", padding: "50px" }}>
+              <h3 style={{ padding: "20px 0" }}>MODERN LIGHTING</h3>
+              <h1 style={{ fontSize: "45px" }}>ONLINE DESIGN SERVICES</h1>
+              <p style={{ padding: "20px 0", fontSize: "18px", fontWeight: "500" }}>Get free help from our certified lighting design experts with any lighting projects</p>
+
+              <Button variant="contained" sx={{ fontSize: "15px", backgroundColor: "black", borderRadius: "0", padding: "10px 25px", ":hover": { backgroundColor: "white", color: "black" } }}>Make An Appointment</Button>
+            </Grid>
           </Grid>
 
-          <Grid size={{ lg: 5, xs: 12, md: 5, sm: 12 }} sx={{ margin: "auto 0", padding: "50px" }}>
-            <h3 style={{ padding: "20px 0" }}>MODERN LIGHTING</h3>
-            <h1 style={{ fontSize: "45px" }}>ONLINE DESIGN SERVICES</h1>
-            <p style={{ padding: "20px 0", fontSize: "18px", fontWeight: "500" }}>Get free help from our certified lighting design experts with any lighting projects</p>
-
-            <Button variant="contained" sx={{ fontSize: "15px", backgroundColor: "black", borderRadius: "0", padding: "10px 25px", ":hover": { backgroundColor: "white", color: "black" } }}>Make An Appointment</Button>
-          </Grid>
-        </Grid>
-
-      </Container>
+        </Container>
       </Box>
       {/* make an appointment section end */}
 
       {/* sofa section start */}
-      <Box sx={{margin:"100px 0"}}>
+      <Box sx={{ margin: "100px 0" }}>
         <Container>
           <Box sx={{ textAlign: 'center', padding: '50px 0' }}>
             <h1 className='topic-title' style={{ fontFamily: 'math', borderBottom: '2px solid black', display: "inline-block" }}>
@@ -799,41 +886,164 @@ const App = () => {
 
       {/* sofa section end */}
 
+      {/* discount banner */}
+      <Box>
+       <a href=""><img src={disbanner} alt="" style={{width:"100%"}} /></a> 
+      </Box>
+      {/* discount banner */}
+
+      {/* product section start */}
+
+      <Box>
+        <Container>
+        <Box sx={{ textAlign: 'center', padding: '50px 0' }}>
+          <h1 className='topic-title' style={{ fontFamily: 'math', borderBottom: '2px solid black', display: "inline-block" }}>
+          Bed Room Furniture
+          </h1>
+        </Box>
+
+        <Box sx={{ width: "100%"}}>
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <a href="">
+            <img
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+              style={{width:"100%"}}
+            /></a>
+            <ImageListItemBar position="below" title={item.author} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+        </Container>
+      </Box>
+      {/* product section end */}
 
       {/* service section start */}
-      
-        <Box sx={{backgroundColor:"#0000001f"}}>
+
+      <Box sx={{ backgroundColor: "#0000001f", margin: "100px 0" }}>
         <Container>
           <Grid container spacing={2}>
-            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{textAlign:"center",padding:"40px 8px",":hover":{backgroundColor:"black",color:"white","& .icon-hover":{backgroundColor:"white",color:"black"},transition:"0.3s"}}}>
-              <HomeIcon className='icon-hover' sx={{padding:"5px",fontSize:"50px",borderRadius:"50%",color:"white",backgroundColor:"black"}}/>
-              <h4 style={{padding: "15px 0",fontSize:"18px",fontFamily:"cursive"}}>Free Shipping</h4>
-              <p style={{fontWeight:"600"}}>We offer fast, free shipping straight to your doorstep.</p>
+            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{ textAlign: "center", padding: "40px 8px", ":hover": { backgroundColor: "black", color: "white", "& .icon-hover": { backgroundColor: "white", color: "black" }, transition: "0.3s" } }}>
+              <HomeIcon className='icon-hover' sx={{ padding: "5px", fontSize: "50px", borderRadius: "50%", color: "white", backgroundColor: "black" }} />
+              <h4 style={{ padding: "15px 0", fontSize: "18px", fontFamily: "cursive" }}>Free Shipping</h4>
+              <p style={{ fontWeight: "600" }}>We offer fast, free shipping straight to your doorstep.</p>
             </Grid>
 
-            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{textAlign:"center",padding:"40px 8px",":hover":{backgroundColor:"black",color:"white","& .icon-hover":{backgroundColor:"white",color:"black"},transition:"0.3s"}}}>
-              <PaymentIcon className='icon-hover' sx={{padding:"5px",fontSize:"50px",borderRadius:"50%",color:"white",backgroundColor:"black"}}/>
-              <h4 style={{padding: "15px 0",fontSize:"18px",fontFamily:"cursive"}}>Securety Payments</h4>
-              <p style={{fontWeight:"600"}}>Your transactions are protected with advanced security.</p>
+            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{ textAlign: "center", padding: "40px 8px", ":hover": { backgroundColor: "black", color: "white", "& .icon-hover": { backgroundColor: "white", color: "black" }, transition: "0.3s" } }}>
+              <PaymentIcon className='icon-hover' sx={{ padding: "5px", fontSize: "50px", borderRadius: "50%", color: "white", backgroundColor: "black" }} />
+              <h4 style={{ padding: "15px 0", fontSize: "18px", fontFamily: "cursive" }}>Securety Payments</h4>
+              <p style={{ fontWeight: "600" }}>Your transactions are protected with advanced security.</p>
             </Grid>
 
-            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{textAlign:"center",padding:"40px 8px",":hover":{backgroundColor:"black",color:"white","& .icon-hover":{backgroundColor:"white",color:"black"},transition:"0.3s"}}}>
-              <AllInclusiveIcon className='icon-hover' sx={{padding:"5px",fontSize:"50px",borderRadius:"50%",color:"white",backgroundColor:"black"}}/>
-              <h4 style={{padding: "15px 0",fontSize:"18px",fontFamily:"cursive"}}>14-Day Returns</h4>
-              <p style={{fontWeight:"600"}}>Hassle-free 14-day returns on all orders.</p>
+            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{ textAlign: "center", padding: "40px 8px", ":hover": { backgroundColor: "black", color: "white", "& .icon-hover": { backgroundColor: "white", color: "black" }, transition: "0.3s" } }}>
+              <AllInclusiveIcon className='icon-hover' sx={{ padding: "5px", fontSize: "50px", borderRadius: "50%", color: "white", backgroundColor: "black" }} />
+              <h4 style={{ padding: "15px 0", fontSize: "18px", fontFamily: "cursive" }}>14-Day Returns</h4>
+              <p style={{ fontWeight: "600" }}>Hassle-free 14-day returns on all orders.</p>
             </Grid>
 
-            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{textAlign:"center",padding:"40px 8px",":hover":{backgroundColor:"black",color:"white","& .icon-hover":{backgroundColor:"white",color:"black"},transition:"0.3s"}}}>
-              <SupportAgentIcon className='icon-hover' sx={{padding:"5px",fontSize:"50px",borderRadius:"50%",color:"white",backgroundColor:"black"}}/>
-              <h4 style={{padding: "15px 0",fontSize:"18px",fontFamily:"cursive"}}>24/7 Support</h4>
-              <p style={{fontWeight:"600"}}>Need help? Our support team is available around the clock</p>
+            <Grid size={{ lg: 3, xs: 12, md: 3, sm: 6 }} sx={{ textAlign: "center", padding: "40px 8px", ":hover": { backgroundColor: "black", color: "white", "& .icon-hover": { backgroundColor: "white", color: "black" }, transition: "0.3s" } }}>
+              <SupportAgentIcon className='icon-hover' sx={{ padding: "5px", fontSize: "50px", borderRadius: "50%", color: "white", backgroundColor: "black" }} />
+              <h4 style={{ padding: "15px 0", fontSize: "18px", fontFamily: "cursive" }}>24/7 Support</h4>
+              <p style={{ fontWeight: "600" }}>Need help? Our support team is available around the clock</p>
             </Grid>
           </Grid>
-          </Container>
-        </Box>
-      
+        </Container>
+      </Box>
+
       {/* service section end */}
       {/* main end */}
+
+      {/* footer start */}
+
+      <Box sx={{ backgroundColor: "black", padding: "80px 0" }}>
+        <Container>
+          <Box className="footer-detail">
+            <Grid container spacing={2}>
+
+              <Grid size={3} sx={{ borderRight: "1px solid white" }}>
+                <Box>
+                  <img src={logo} alt="" style={{ width: "50%" }} />
+                </Box>
+                <Box className="Contact-detail">
+                  <h1 style={{ color: "white", fontSize: "28px", padding: "25px 0 10px 0", display: "inline-block", borderBottom: "1px solid white" }}>Contact Us :</h1>
+
+                  <span style={{ display: "flex", color: "white", alignItems: "center", paddingTop: "5px" }}> <CallIcon sx={{ padding: "5px", fontSize: "30px", marginRight: "5px", color: "black", backgroundColor: "white", borderRadius: "50%" }} /> +91 9796586501</span>
+
+                  <span style={{ display: "flex", color: "white", alignItems: "center", paddingTop: "5px" }}> <EmailIcon sx={{ padding: "5px", fontSize: "30px", marginRight: "5px", color: "black", backgroundColor: "white", borderRadius: "50%" }} /> luxiloom@gmail.com</span>
+
+                  <h2 style={{ color: "white", fontSize: "28px", padding: "10px 0 10px 0", display: "inline-block", borderBottom: "1px solid white" }}>Address :</h2>
+                  <span style={{ display: "flex", color: "white", alignItems: "center", paddingTop: "5px" }}> <BusinessIcon sx={{ padding: "5px", fontSize: "30px", marginRight: "5px", color: "black", backgroundColor: "white", borderRadius: "50%" }} /> B-45,Central Park,Surat</span>
+                </Box>
+
+              </Grid>
+
+              <Grid size={3}>
+                <h3 style={{ padding: "0px 5px", display: "inline-block", color: "black", fontSize: "28px", marginBottom: "30px", backgroundColor: "white" }}>Information</h3>
+                <ul>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Customer Stories</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Events</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Featured Projects</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>About Us</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>FAQs</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Gifting</a></li>
+                </ul>
+              </Grid>
+
+
+
+              <Grid size={3}>
+                <h3 style={{ padding: "0px 5px", display: "inline-block", color: "black", fontSize: "28px", marginBottom: "30px", backgroundColor: "white" }}>Category</h3>
+                <ul>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Furniture</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Lighting</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Decor</a></li>
+
+                </ul>
+              </Grid>
+
+              <Grid size={3}>
+                <h3 style={{ padding: "0px 5px", display: "inline-block", color: "black", fontSize: "28px", marginBottom: "30px", backgroundColor: "white" }}>Our Stores</h3>
+                <ul>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Jodhpur</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Hyderabad</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Bengaluru</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Ahmedabad</a></li>
+                  <li style={{ padding: "8px 0" }}><a href="" style={{ color: "white", fontFamily: "system-ui", fontSize: "16px", fontWeight: "500" }}>Nagpur</a></li>
+
+                </ul>
+              </Grid>
+            </Grid>
+
+
+          </Box>
+          <hr style={{ color: "white", margin: "15px 0" }} />
+
+          <Box>
+            <Box className="social-app" sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+
+              <a href="" style={{ padding: "0 15px" }}><InstagramIcon sx={{
+                color: "white", fontSize: "35px", ":hover": { color: "blueviolet" }
+              }} /></a>
+              <a href="" style={{ padding: "0 15px" }}><WhatsAppIcon sx={{ color: "white", fontSize: "35px", ":hover": { color: "blueviolet" } }} /></a>
+              <a href="" style={{ padding: "0 15px" }}><LinkedInIcon sx={{ color: "white", fontSize: "35px", ":hover": { color: "blueviolet" } }} /></a>
+              <a href="" style={{ padding: "0 15px" }}> <PinterestIcon sx={{ color: "white", fontSize: "35px", ":hover": { color: "blueviolet" } }} /></a>
+              <a href="" style={{ padding: "0 15px" }}><TelegramIcon sx={{ color: "white", fontSize: "35px", ":hover": { color: "blueviolet" } }} /></a>
+              <a href="" style={{ padding: "0 15px" }}><YouTubeIcon sx={{ color: "white", fontSize: "35px", ":hover": { color: "blueviolet" } }} /></a>
+
+            </Box>
+            <Box className="last-detail" sx={{ textAlign: "center", paddingTop: "10px" }}>
+              <p style={{ color: "white" }}>Copyright 2025 <span style={{ fontFamily: "cursive", fontWeight: "600", padding: "0 8px" }}>Luxi-Loom</span> PVT. LTD. | <a href="" style={{ color: "white" }}>Terms & Conditions</a> | <a href="" style={{ color: "white" }}>Shipping & Return</a> | <a href="" style={{ color: "white" }}>Sitemap</a></p>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+
+      {/* footer end */}
 
     </>
   )
